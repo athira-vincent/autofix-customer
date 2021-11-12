b7563af36ec189d0152476e2e04abb8f8652d03b
Athira <athira.ardhet@techwarelab.com>
2021-11-12 11:10:37 +0530

    Walk Through

--- lib/UI/WelcomeScreens/Splash/splash_screen.dart
+++ lib/UI/WelcomeScreens/Splash/splash_screen.dart
@@ -6,6 +6,7 @@ import 'package:auto_fix/Constants/grapgh_ql_client.dart';
 import 'package:auto_fix/Constants/shared_pref_keys.dart';
 import 'package:auto_fix/UI/Customer/Home/home_screen.dart';
 import 'package:auto_fix/UI/Customer/Login/login_screen.dart';
+import 'package:auto_fix/UI/WelcomeScreens/WalkThrough/walk_through_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -32,6 +33,9 @@ class _SplashScreenState extends State<SplashScreen> {
   Future _changeScreen() async {
     SharedPreferences _shdPre = await SharedPreferences.getInstance();
     bool? _isLoggedin = _shdPre.getBool(SharedPrefKeys.isUserLoggedIn);
+    bool? isWalked = _shdPre.getBool(SharedPrefKeys.isWalked);
+
+    print("is isWalked in=======$isWalked");
 
     var _token = _shdPre.getString(SharedPrefKeys.token);
     if (_token == null || _token == "") {
@@ -39,6 +43,14 @@ class _SplashScreenState extends State<SplashScreen> {
     } else {
       GqlClient.I.config(token: _token);
     }
+
+    if (isWalked==null || !isWalked) {
+      print('WalkThroughPage');
+      Timer(Duration(seconds: 3), () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (BuildContext context) => WalkThroughPages())));
+    }
+
+
 
     if (_isLoggedin == null) {
       Navigator.pushReplacement(context,
