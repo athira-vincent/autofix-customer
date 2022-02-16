1456e04a3fd8ecf33d226e7e17e78f919a966dd3
Athira <athira.ardhet@techwarelab.com>
2022-02-16 16:42:16 +0530

    working commit

--- lib/UI/WelcomeScreens/WalkThrough/walk_through_screen.dart
+++ lib/UI/WelcomeScreens/WalkThrough/walk_through_screen.dart
@@ -26,27 +26,126 @@ class WalkThroughPagesState extends State<WalkThroughPages> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Stack(
           children: [
-            IndicatorWidget(isFirst: true,isSecond: false,isThird: false,),
-            PageView.builder(
-              controller: pageController,
-              itemCount: slides.length,
-              onPageChanged: (val) {
-                setState(() {
-                  currentIndex = val;
-                });
-              },
-              itemBuilder: (context, index) {
-                return SliderTile(
-                  iconPath: slides[index].iconPath,
-                  smallTitle: slides[index].smallTitle,
-                  largeTitle: slides[index].largeTitle,
-                  description: slides[index].description,
-                );
-              },
+
+            IndicatorWidget(isFirst: true,isSecond: false,isThird: false,isFourth: false,),
+
+            Positioned(
+              top: size.height * 0.100,
+              child: Container(
+                margin: EdgeInsets.only(),
+                child: currentIndex == 0
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+
+
+                          InkWell(
+                            child: GestureDetector(
+                              onTap: () {
+                                pageController.animateToPage(currentIndex + 1,
+                                    duration: Duration(milliseconds: 400),
+                                    curve: Curves.linear);
+                              },
+                              child: Container(
+                                width: 74.3,
+                                height: 26.5,
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 5.5,
+                                ),
+                                color: Colors.white,
+                                child: Center(
+                                  child: Text(
+                                  "Next",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15.5,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'Corbel_Bold'),
+                                ),
+                            ),
+                         ),
+                        ),
+                     ),
+                      InkWell(
+                        child: GestureDetector(
+                          onTap: () {
+                            //pageController.animateToPage(currentIndex - 1, duration: Duration(milliseconds: 400), curve: Curves.linear);
+                            setIswalked();
+                          },
+                            child: Container(
+                              width: 74.3,
+                              height: 26.5,
+                              padding: EdgeInsets.symmetric(
+                                vertical: 5.5,
+                                ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.white, width: .3)),
+                              child: Center(
+                                child: Text(
+                                  "Skip",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15.5,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Corbel_Bold'),
+                              ),
+                            ),
+                          ),
+                      ),
+                    ),
+                  ],
+                )
+                    : InkWell(
+                  child: GestureDetector(
+                    onTap: () {
+                      setIswalked();
+                    },
+                    child: Container(
+                      width: 10,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(.5),
+                          border: Border.all(color: Colors.white, width: .3)),
+                      height: 20.5,
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.5,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Corbel_Bold'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: size.height * 0.09,left: size.width * 0.118,right: size.width * 0.118),
+              child: PageView.builder(
+                controller: pageController,
+                itemCount: slides.length,
+                onPageChanged: (val) {
+                  setState(() {
+                    currentIndex = val;
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return SliderTile(
+                    iconPath: slides[index].iconPath,
+                    smallTitle: slides[index].smallTitle,
+                    largeTitle: slides[index].largeTitle,
+                    description: slides[index].description,
+                  );
+                },
+              ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
@@ -105,7 +204,7 @@ class WalkThroughPagesState extends State<WalkThroughPages> {
                             child: Text(
                               "Skip",
                               style: TextStyle(
-                                  color: Colors.white,
+                                  color: CustColors.azure,
                                   fontSize: 15.5,
                                   fontWeight: FontWeight.w600,
                                   fontFamily: 'Corbel_Bold'),
@@ -171,36 +270,37 @@ class SliderTile extends StatelessWidget {
 
   final smallTitleStyle = TextStyle(
     color: CustColors.greyish_purple,
-    fontSize: ScreenSize().setValueFont(23.3),
+    fontSize: 23.3,
     height: 1.5,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w300,
     fontFamily: 'Corbel_Bold',
   );
 
   final largeTitleStyle = TextStyle(
     color: CustColors.azure,
-    fontSize: ScreenSize().setValueFont(29.5),
+    fontSize: 36.7,
     fontFamily: 'Corbel_Bold',
     fontWeight: FontWeight.bold,
   );
 
   final descriptionStyle = TextStyle(
     color: CustColors.greyish_purple,
-    fontSize: ScreenSize().setValueFont(29.5),
+    fontSize: 13.3,
     fontFamily: 'Corbel_Bold',
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeight.w100,
   );
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Column(
       //alignment: Alignment.bottomCenter,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-
         Container(
-            /*height: MediaQuery.of(context).size.height * 0.734,
-            width: MediaQuery.of(context).size.width * 0.43,
-            margin: EdgeInsets.only(),*/
+            height: size.height * 0.43,
+            width: size.width * 0.734,
             child: Image.asset(iconPath)
         ),
 
