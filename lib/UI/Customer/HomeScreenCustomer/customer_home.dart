b6d0bdf9f4bb033683769902a8dfc902f1b01a0b
user <maria>
2022-03-01 14:18:46 +0530

    Changes

--- lib/UI/Customer/HomeScreenCustomer/customer_home.dart
+++ lib/UI/Customer/HomeScreenCustomer/customer_home.dart
@@ -23,6 +23,17 @@ class _HomeCustomerUIScreenState extends State<HomeCustomerUIScreen> {
   TextEditingController searchController = new TextEditingController();
   String? filter;
 
+   List<Choice> choices = const <Choice>[
+    const Choice(title: 'Home', icon: Icons.home),
+    const Choice(title: 'Contact', icon: Icons.contacts),
+    const Choice(title: 'Map', icon: Icons.map),
+    const Choice(title: 'Phone', icon: Icons.phone),
+    const Choice(title: 'Camera', icon: Icons.camera_alt),
+    const Choice(title: 'Setting', icon: Icons.settings),
+    const Choice(title: 'Album', icon: Icons.photo_album),
+    const Choice(title: 'WiFi', icon: Icons.wifi),
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -144,6 +155,14 @@ class _HomeCustomerUIScreenState extends State<HomeCustomerUIScreen> {
 
 
 }
+
+class Choice {
+  const Choice({required this.title, required this.icon});
+  final String title;
+  final IconData icon;
+}
+
+
 
 class MyBehavior extends ScrollBehavior {
   @override
