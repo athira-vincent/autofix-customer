9d28bed2b217809887ba4f1a013e09882e5efab7
user <maria>
2022-03-02 15:42:59 +0530

    Changes

--- lib/UI/Customer/HomeScreenCustomer/customer_home.dart
+++ lib/UI/Customer/HomeScreenCustomer/customer_home.dart
@@ -1,6 +1,8 @@
 import 'package:auto_fix/Constants/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../../../Constants/cust_colors.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -45,11 +47,66 @@ class _HomeCustomerUIScreenState extends State<HomeCustomerUIScreen> {
     const Choice(title: 'WiFi', icon: Icons.wifi),
   ];
 
+  String location ='Null, Press Button';
+  String Address = 'search';
 
-   @override
+
+
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _getCurrentCustomerLocation();
+
+  }
+
+  Future<void> _getCurrentCustomerLocation() async {
+    Position position = await _getGeoLocationPosition();
+    location ='Lat: ${position.latitude} , Long: ${position.longitude}';
+    print(location);
+    GetAddressFromLatLong(position);
+  }
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+
+
+  Future<void> GetAddressFromLatLong(Position position)async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    Address = '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
 
   }
 
@@ -155,9 +212,6 @@ class _HomeCustomerUIScreenState extends State<HomeCustomerUIScreen> {
       ],
     );
   }
-
-
-
 
   Widget emergencyService() {
     return Column(
@@ -478,6 +532,8 @@ class _HomeCustomerUIScreenState extends State<HomeCustomerUIScreen> {
       ),
     );
   }
+
+
 
 
 }
