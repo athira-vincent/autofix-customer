2fbe48f97d36bcb0fcd6e872163b0a98f98c9027
user <maria>
2022-04-11 12:54:59 +0530

    Changes

--- lib/UI/Customer/SideBar/MyVehicles/cust_my_vehicles.dart
+++ lib/UI/Customer/SideBar/MyVehicles/cust_my_vehicles.dart
@@ -1,7 +1,9 @@
 import 'package:auto_fix/Constants/cust_colors.dart';
 import 'package:auto_fix/Constants/shared_pref_keys.dart';
 import 'package:auto_fix/Constants/styles.dart';
+import 'package:auto_fix/Widgets/input_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CustomerMyVehicleScreen extends StatefulWidget {
@@ -26,6 +28,24 @@ class _CustomerMyVehicleScreenState extends State<CustomerMyVehicleScreen> {
     "https://firebasestorage.googleapis.com/v0/b/autofix-336509.appspot.com/o/SupportChatImages%2FsparepartImage1.png?alt=media&token=0130eb9b-662e-4c1c-b8a1-f4232cbba284",
     'https://firebasestorage.googleapis.com/v0/b/autofix-336509.appspot.com/o/SupportChatImages%2FsparepartImage2.png?alt=media&token=419e2555-5c26-4295-8201-6c78f1ed563e',
   ];
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autoValidate = AutovalidateMode.disabled;
+
+  TextEditingController _brandController = TextEditingController();
+  FocusNode _brandFocusNode = FocusNode();
+
+  TextEditingController _modelController = TextEditingController();
+  FocusNode _modelFocusNode = FocusNode();
+
+  TextEditingController _engineTypeController = TextEditingController();
+  FocusNode _engineTypeFocusNode = FocusNode();
+
+  TextEditingController _yearController = TextEditingController();
+  FocusNode _yearControllerFocusNode = FocusNode();
+
+  TextEditingController _lastMaintenanceController = TextEditingController();
+  FocusNode _lastMaintenanceFocusNode = FocusNode();
 
 
   @override
@@ -89,6 +109,7 @@ class _CustomerMyVehicleScreenState extends State<CustomerMyVehicleScreen> {
                             )
                         ),
                       ),
+                      bodyUi(),
                     ],
                   ),
                   Visibility(
@@ -227,6 +248,369 @@ class _CustomerMyVehicleScreenState extends State<CustomerMyVehicleScreen> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget bodyUi() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Form(
+            autovalidateMode: _autoValidate,
+            key: _formKey,
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 20, right: 20,top:20, bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: CustColors.pale_grey,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(0),
+                        bottomRight: Radius.circular(0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        children: [
+                          brandTextSelection(),
+                          modelTextSelection(),
+                          engineTypeTextSelection(),
+                          yearTypeTextSelection(),
+                          lastMaintenanceTextSelection(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+        ),
+      ],
+    );
+  }
+
+
+
+  Widget brandTextSelection() {
+    return  Container(
+      margin: EdgeInsets.only(top: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Brand",
+                  style: Styles.textLabelTitle,
+                ),
+                Spacer(),
+
+              ],
+            ),
+          ),
+          TextFormField(
+            textAlignVertical: TextAlignVertical.center,
+            maxLines: 1,
+            style: Styles.textLabelSubTitle,
+            focusNode: _brandFocusNode,
+            enabled: false,
+            keyboardType: TextInputType.name,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp('[a-zA-Z ]')),
+            ],
+            validator: InputValidator(
+                ch : 'Brand name' ).nameCheckingWithNumeric,
+            controller: _brandController,
+            cursorColor: CustColors.whiteBlueish,
+            decoration: InputDecoration(
+              isDense: true,
+              hintText:
+              "",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 12.8,
+                horizontal: 0.0,
+              ),
+              errorStyle: Styles.textLabelSubTitleRed,
+              hintStyle: Styles.textLabelSubTitle,),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget modelTextSelection() {
+    return  Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Model",
+                  style: Styles.textLabelTitle,
+                ),
+                Spacer(),
+
+              ],
+            ),
+          ),
+          TextFormField(
+            textAlignVertical: TextAlignVertical.center,
+            maxLines: 1,
+            style: Styles.textLabelSubTitle,
+            focusNode: _modelFocusNode,
+            keyboardType: TextInputType.name,
+            enabled: false,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp('[a-zA-Z ]')),
+            ],
+            validator: InputValidator(
+                ch :
+                'Model name'  ).nameCheckingWithNumeric,
+            controller: _modelController,
+            cursorColor: CustColors.whiteBlueish,
+            decoration: InputDecoration(
+              isDense: true,
+              hintText:
+              "",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 12.8,
+                horizontal: 0.0,
+              ),
+              errorStyle: Styles.textLabelSubTitleRed,
+              hintStyle: Styles.textLabelSubTitle,),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget engineTypeTextSelection() {
+    return  Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+
+            "Select Engine Type",
+            style: Styles.textLabelTitle,
+          ),
+          TextFormField(
+            textAlignVertical: TextAlignVertical.center,
+            maxLines: 1,
+            style: Styles.textLabelSubTitle,
+            focusNode: _engineTypeFocusNode,
+            enabled: false,
+            keyboardType: TextInputType.name,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp('[a-zA-Z ]')),
+            ],
+            validator: InputValidator(
+                ch :
+                'Engine Type' ).nameCheckingWithNumericAndBracket,
+            controller: _engineTypeController,
+            cursorColor: CustColors.whiteBlueish,
+            decoration: InputDecoration(
+              isDense: true,
+              hintText:
+              "",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 12.8,
+                horizontal: 0.0,
+              ),
+              errorStyle: Styles.textLabelSubTitleRed,
+              hintStyle: Styles.textLabelSubTitle,),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget yearTypeTextSelection() {
+    return  Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+
+            "Select Year",
+            style: Styles.textLabelTitle,
+          ),
+          TextFormField(
+            textAlignVertical: TextAlignVertical.center,
+            maxLines: 1,
+            style: Styles.textLabelSubTitle,
+            focusNode: _yearControllerFocusNode,
+            keyboardType: TextInputType.name,
+            enabled: false,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp('[a-zA-Z ]')),
+            ],
+            validator: InputValidator(
+                ch :
+                'Year' ).nameCheckingWithNumeric,
+            controller: _yearController,
+            cursorColor: CustColors.whiteBlueish,
+            decoration: InputDecoration(
+              isDense: true,
+              hintText:
+              "",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 12.8,
+                horizontal: 0.0,
+              ),
+              errorStyle: Styles.textLabelSubTitleRed,
+              hintStyle: Styles.textLabelSubTitle,),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget lastMaintenanceTextSelection() {
+    return  Container(
+      margin: EdgeInsets.only(top: 20,bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Last maintenance",
+            style: Styles.textLabelTitle,
+          ),
+          TextFormField(
+            textAlignVertical: TextAlignVertical.center,
+            maxLines: 1,
+            style: Styles.textLabelSubTitle,
+            focusNode: _lastMaintenanceFocusNode,
+            keyboardType: TextInputType.name,
+            enabled: false,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp('[a-zA-Z ]')),
+            ],
+            validator: InputValidator(
+                ch :
+                'Last maintenance date' ).nameCheckingWithNumeric,
+            controller: _lastMaintenanceController,
+            cursorColor: CustColors.whiteBlueish,
+            decoration: InputDecoration(
+              isDense: true,
+              hintText:
+              "",
+              border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustColors.greyish,
+                  width: .5,
+                ),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 12.8,
+                horizontal: 0.0,
+              ),
+              errorStyle: Styles.textLabelSubTitleRed,
+              hintStyle: Styles.textLabelSubTitle,),
+          ),
+        ],
+      ),
     );
   }
 
