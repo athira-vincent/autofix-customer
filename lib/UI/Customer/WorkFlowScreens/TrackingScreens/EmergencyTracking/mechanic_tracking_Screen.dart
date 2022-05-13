fb894c8181cc9198a3f5845b019b1f42e2e27735
maria <JiP4J2JmyGw2PeXrXQdn>
2022-05-13 12:23:52 +0530

    Latest Changes

--- lib/UI/Customer/WorkFlowScreens/TrackingScreens/EmergencyTracking/mechanic_tracking_Screen.dart
+++ lib/UI/Customer/WorkFlowScreens/TrackingScreens/EmergencyTracking/mechanic_tracking_Screen.dart
@@ -13,6 +13,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:ui' as ui;
 import 'package:location/location.dart' as loc;
+import 'dart:math' show cos, sqrt, asin;
+
 
 class MechanicTrackingScreen extends StatefulWidget {
 
@@ -53,6 +55,9 @@ class _MechanicTrackingScreenState extends State<MechanicTrackingScreen> {
       -122.391386000,),
     zoom: 25,
   );
+  double totalDistance = 0.0;
+  String? _placeDistance;
+
 
 
   @override
@@ -170,6 +175,10 @@ class _MechanicTrackingScreenState extends State<MechanicTrackingScreen> {
     }
     addPolyLine(polylineCoordinates);
 
+    distanceCalculation(polylineCoordinates);
+
+
+
   }
 
 
@@ -186,6 +195,25 @@ class _MechanicTrackingScreenState extends State<MechanicTrackingScreen> {
     setState(() {});
     _googleMap = _googleMapIntegrate();
   }
+
+
+  distanceCalculation(List<LatLng> polylineCoordinates)
+  {
+    for (int i = 0; i < polylineCoordinates.length - 1; i++) {
+      totalDistance += _coordinateDistance(
+        polylineCoordinates[i].latitude,
+        polylineCoordinates[i].longitude,
+        polylineCoordinates[i + 1].latitude,
+        polylineCoordinates[i + 1].longitude,
+      );
+    }
+
+    setState(() {
+      _placeDistance = totalDistance.toStringAsFixed(2);
+      print('DISTANCE ===== : $_placeDistance km');
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -495,6 +523,15 @@ class _MechanicTrackingScreenState extends State<MechanicTrackingScreen> {
         ),
       ),
     );
+  }
+
+  double _coordinateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
   }
 
 
