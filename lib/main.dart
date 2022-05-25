0f4fe7a409c8600f560308afcaff964cbfd28814
admin <ZXz1JKQfJ2dh3yxZCDSx>
2022-05-25 14:39:15 +0530

    Latest Changes

--- lib/main.dart
+++ lib/main.dart
@@ -125,7 +125,6 @@ class _MyAppState extends State<MyApp> {
 
                  home: SplashScreen(),
 
-
                  // home: MechanicStartServiceScreen(),
 
                 //home: MechanicWorkCompletedScreen(),
