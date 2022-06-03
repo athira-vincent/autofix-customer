98de4d375a9f07609173ff18ee1b77abb4e26b01
admin <ZXz1JKQfJ2dh3yxZCDSx>
2022-06-03 14:59:18 +0530

    Latest Changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:auto_fix/Constants/cust_colors.dart';
-import 'package:auto_fix/UI/Mechanic/WorkFlowScreens/CustomerApproved/customer_approved_screen.dart';
 import 'package:auto_fix/UI/WelcomeScreens/Login/CompleteProfile/Mechanic/regular_service_list.dart';
 import 'package:auto_fix/UI/WelcomeScreens/Login/CompleteProfile/Mechanic/work_selection_screen.dart';
 import 'package:auto_fix/UI/WelcomeScreens/Login/Signin/login_screen.dart';
@@ -90,7 +89,7 @@ class _MyAppState extends State<MyApp> {
                   primaryColor: Colors.white,
                 ),
 
-                home: CustomerApprovedScreen(),
+                home: SplashScreen(),
               );
             },
           );
