731636cfb54bc3594fadebc694047e6bc5793964
vaisakh <vaisakh@techwarelab.com>
2022-06-07 12:25:52 +0530

    sample commit

--- lib/main.dart
+++ lib/main.dart
@@ -88,6 +88,7 @@ class _MyAppState extends State<MyApp> {
                   //brightness: Brightness.light,
                   primaryColor: Colors.white,
                 ),
+
                 home: SplashScreen(),
               );
             },
