c99f9b490332959ab510e89b9fd86da0ea5e7572
admin <ZXz1JKQfJ2dh3yxZCDSx>
2022-06-11 23:20:12 +0530

    Latest Changes

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,6 @@ class _MyAppState extends State<MyApp> {
                 theme: ThemeData(
                   primaryColor: Colors.white,
                 ),
-
                 home: SplashScreen(),
 
                 // home: MechanicStartServiceScreen(),
