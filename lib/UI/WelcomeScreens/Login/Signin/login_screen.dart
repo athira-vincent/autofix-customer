d678555c910a3489ff96ae31ecb3b3148262050d
admin <ZXz1JKQfJ2dh3yxZCDSx>
2022-06-14 17:50:43 +0530

    Latest Changes

--- lib/UI/WelcomeScreens/Login/Signin/login_screen.dart
+++ lib/UI/WelcomeScreens/Login/Signin/login_screen.dart
@@ -487,12 +487,12 @@ class _LoginScreenState extends State<LoginScreen> {
       if (value.status == "error") {
         setState(() {
           _isLoading = false;
-          SnackBarWidget().setMaterialSnackBar("Login Successful",_scaffoldKey);
+          SnackBarWidget().setSnackBar(value.message.toString().split(":").last,context);
         });
       } else {
         setState(() {
           _isLoading = false;
-          SnackBarWidget().setMaterialSnackBar("Login Successful",_scaffoldKey);
+          SnackBarWidget().setSnackBar("Login Successful",context);
           if(value.data!.signIn!.user!.userTypeId.toString() == "1"){
             _signinBloc.userDefault(
                 value.data!.signIn!.token.toString(),
@@ -535,7 +535,7 @@ class _LoginScreenState extends State<LoginScreen> {
           _isLoading = false;
           socialLoginIsLoading = false;
           //SnackBarWidget().setMaterialSnackBar(value.message.toString().split(":").last,_scaffoldKey);
-          SnackBarWidget().setSnackBar(value.message.toString(),context);
+          SnackBarWidget().setMaterialSnackBar(value.message.toString(),_scaffoldKey);
         });
       } else {
         print('value.status succes 11111 >>>>>>>>>>>>>>>>+++${value.data!.socialLogin!.user!.userTypeId}');
@@ -573,7 +573,7 @@ class _LoginScreenState extends State<LoginScreen> {
           }
           else if(value.data!.socialLogin!.user!.userTypeId.toString() == "3"){
             print('Please login through Relex App >>>>>>>>>>>>>>>>+++');
-            SnackBarWidget().setSnackBar('Please login through Relex App',context);
+            SnackBarWidget().setMaterialSnackBar('Please login through Relex App',_scaffoldKey);
           }
         });
       }
