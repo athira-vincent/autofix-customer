e01a8adf69030724fc8d1ea488d6c80efafe9633
admin <ZXz1JKQfJ2dh3yxZCDSx>
2022-06-16 16:55:58 +0530

    Latest Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:auto_fix/Constants/cust_colors.dart';
+import 'package:auto_fix/Provider/Profile/profile_data_provider.dart';
 import 'package:auto_fix/UI/Customer/SideBar/MyVehicles/cust_my_vehicles.dart';
 import 'package:auto_fix/UI/WelcomeScreens/Splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -90,7 +91,6 @@ class _MyAppState extends State<MyApp> {
               },
             ),
     );
-
     /*return ChangeNotifierProvider(
         create: (context) => LocaleProvider(),
         builder: (context, child) {
