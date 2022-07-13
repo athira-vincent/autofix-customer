b799bff6076a316e1ab27ff52f3e6f48182079a1
HP <Sag96NP6-GpcvNiiZjW1>
2022-07-13 11:25:53 +0530

    design

--- lib/main.dart
+++ lib/main.dart
@@ -113,11 +113,11 @@ import 'UI/Mechanic/SideBar/MyWallet/my_wallet_screen.dart';
                     ),*/
                     // home: MechMobileTrackScreen(bookingId: "1305"),
 
-                    //home:SplashScreen(),
+                    home:SplashScreen(),
 
                    // home: MechanicMyWalletScreen(),
 
-                    home: MechanicMyJobReviewScreen(),
+                    //home: MechanicMyJobReviewScreen(),
 
                   );
                 },
