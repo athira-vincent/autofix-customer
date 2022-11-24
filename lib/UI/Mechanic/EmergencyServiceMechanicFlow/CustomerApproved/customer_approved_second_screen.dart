52688d01928b5dbbc18751375f97ace416fb9e9d
Noel <noel@techwarelab.com>
2022-11-24 19:55:17 +0530

    emergency flow - issue fix

--- lib/UI/Mechanic/EmergencyServiceMechanicFlow/CustomerApproved/customer_approved_second_screen.dart
+++ lib/UI/Mechanic/EmergencyServiceMechanicFlow/CustomerApproved/customer_approved_second_screen.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
 import 'package:auto_fix/Constants/cust_colors.dart';
-import 'package:auto_fix/Constants/shared_pref_keys.dart';
 import 'package:auto_fix/Constants/styles.dart';
 import 'package:auto_fix/UI/Mechanic/EmergencyServiceMechanicFlow/CustomerApproved/additional_time_bloc.dart';
-import 'package:auto_fix/UI/Mechanic/EmergencyServiceMechanicFlow/MechanicWorkComleted/mechanic_work_completed_screen.dart';
 import 'package:auto_fix/UI/Mechanic/EmergencyServiceMechanicFlow/OrderStatusUpdateApi/order_status_update_bloc.dart';
 import 'package:auto_fix/Widgets/mechanicWorkTimer.dart';
-import 'package:auto_fix/Widgets/mechanicWorkTimer02.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
