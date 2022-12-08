075a0c81bc9d7b6662ef8fdeb7962dfa83d7e296
athira.ardhet <athira.ardhet@techwarelab.com>
2022-12-08 12:24:13 +0530

    language translation

--- lib/UI/WelcomeScreens/Login/CompleteProfile/Mechanic/vechicleSpecialization/vehicle_specialization_screen.dart
+++ lib/UI/WelcomeScreens/Login/CompleteProfile/Mechanic/vechicleSpecialization/vehicle_specialization_screen.dart
@@ -9,6 +9,7 @@ import 'package:auto_fix/Widgets/screen_size.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 
 class VehicleSpecializationScreen extends StatefulWidget {
